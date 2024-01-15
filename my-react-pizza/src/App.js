48f486bac0abf77fe0b8908699a72f99b62b7e64
Dash_2 <dasha.semivolos@gmail.com>
2024-01-15 20:31:25 +0300

    props

--- my-react-pizza/src/App.js
+++ my-react-pizza/src/App.js
@@ -18,9 +18,9 @@ function App() {
         </div>
         <h2 class="content__title">Все пиццы</h2>
         <div class="content__items">
-          <PizzaBlock />
-          <PizzaBlock />
-          <PizzaBlock />
+          <PizzaBlock title="Мексиканская" prise={400} />
+          <PizzaBlock title="4 сыра" prise={350} />
+          <PizzaBlock title="Гавайская" prise={420} />
         </div>
       </div>
     </div>
