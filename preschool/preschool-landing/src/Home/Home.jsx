53875b8730eae752fcb2c3719ecedb4469716644
Dash-02 <dasha.semivolos@gmail.com>
2024-04-18 00:43:31 +0300

    home-all-components

--- preschool/preschool-landing/src/Home/Home.jsx
+++ preschool/preschool-landing/src/Home/Home.jsx
@@ -2,15 +2,31 @@ import React from "react";
 import './Home.css';
 import Header from '../Header/Header.jsx';
 import imgArrow from '../assets/img/Arrow.png';
+import imgBooks from '../assets/img/vecteezy_book.png';
+import tg from '../assets/icons/telegramm.svg';
+import whatsapp from '../assets/icons/watsapp.svg';
 
 function Home() {
     return (
         <div className="home">
             <Header />
             <div className="line"></div>
-            <h1>Онлайн-платформа для дошкольного образования</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-            <img src={} alt="arrow" />
+            <div className="container">
+                <div className="content">
+                    <h1>Онлайн-платформа для <span className="yellow-text">дошкольного</span> образования</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                    <img src={imgArrow} alt="arrow" />
+            
+                    <button>Записаться на обучение</button>
+                
+                    <div>
+                        <p>или написать:</p>
+                        <img src={tg} alt="telegram" />
+                        <img src={whatsapp} alt="whatsApp" />
+                    </div>
+                </div>
+                <img className="img-books" src={imgBooks} alt="books" />
+            </div>
         </div>
     )
 }
