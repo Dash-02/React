0b0858a66a75bf6ff51960e0055260ebae7c6613
Dash-02 <dasha.semivolos@gmail.com>
2024-04-21 01:20:19 +0300

    home-position

--- preschool/preschool-landing/src/Home/Home.jsx
+++ preschool/preschool-landing/src/Home/Home.jsx
@@ -10,25 +10,33 @@ function Home() {
     return (
         <section className="home">
             <Header />
-            {/* <div className="line"></div> */}
             <div className="container">
                 <div className="content">
-                    <h1>Онлайн-платформа для <span className="yellow-text">дошкольного</span> образования</h1>
-                    <div>
-                        <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</span>
+                    <div className="text_wrapper">
+                        <h1>Онлайн-платформа для <span className="yellow-text">дошкольного</span> образования</h1>
+                        
+                            <span>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                            </span>
+                        
+                        {/* <img src={imgArrow} alt="arrow" className="img-arrow" /> */}
                     </div>
-                    <img src={imgArrow} alt="arrow" />
-            
-                    <div className="wrapper_social_media">
-                        <button>Записаться на обучение</button>
-                
-                        <div className="social_media">
-                            <span>или написать:</span>
-                            <a href="#"><img src={tg} alt="telegram" /></a>
-                            <a href="#"><img src={whatsapp} alt="whatsApp" /></a>
+                    <div className="content-wrapper">
+                    <img src={imgArrow} alt="arrow" className="img-arrow" />
+                        
+                        <div className="wrapper_social_media">
+                            <button>Записаться на обучение</button>
+                            <div className="social_media">
+                                <span>или написать:</span>
+                                <a href="#"><img src={tg} alt="telegram" /></a>
+                                <a href="#"><img src={whatsapp} alt="whatsApp" /></a>
+                            </div>
+                            
                         </div>
+                    {/* <img src={imgArrow} alt="arrow" className="img-arrow" /> */}
                     </div>
                 </div>
+                
                 <img className="img-books" src={imgBooks} alt="books" />
             </div>
         </section>
